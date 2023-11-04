6f7f3d38d7bd0c44ed3ebed989c3f65f8feb2293
ikashnitsky <ilya.kashnitsky@gmail.com>
2023-11-04 12:01:20 +0100

    map proj

---
+++ 2023/map-proj/export-figures.R
@@ -0,0 +1,19 @@
+
+# save plots for arrangind a social media post
+
+gd_n2_main %>%
+    ggplot() +
+    geom_sf(fill = "#F48FB1", color = NA)+
+    geom_sf(data = bord, color = "#C2185B", size = .5)+
+    coord_sf(crs = 3857)+
+    theme_void()
+
+ggsave("~/Downloads/merc.pdf", width = 4, height = 4)
+
+gd_n2_main_laea %>%
+    ggplot() +
+    geom_sf(fill = "#DCE775", color = NA)+
+    geom_sf(data = bord, color = "#AFB42B", size = .5)+
+    theme_void()
+
+ggsave("~/Downloads/laea.pdf", width = 4, height = 4)
