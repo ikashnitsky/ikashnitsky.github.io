8778d85ea99c36bedb8fd1d440c0e538ac4e9457
Ilya Kashnitsky <ilya.kashnitsky@gmail.com>
2024-12-17 13:16:32 +0100

    prep rotate-damn-plot post

---
+++ 2024/rotate-damn-plot/replot.R
@@ -0,0 +1,63 @@
+#===============================================================================
+# 2024-12-12 -- blog
+# Rotate the damn plot
+# Ilya Kashnitsky, ilya.kashnitsky@gmail.com
+#===============================================================================
+
+
+
+library(tidyverse)
+
+# Points digitized manually using
+# https://web.eecs.utk.edu/~dcostine/personal/PowerDeviceLib/DigiTest/index.html
+digi <- read_csv("2024/rotate-damn-plot/data.csv", col_names = c("x", "happiness"))
+
+
+df <- crossing(
+    country = c("United Kingdom", "Estonia", "Denmark", "Netherlands", "Czech Republic", "Finland", "Austria") |> as_factor(),
+    group = c("18-29", "30-39", "40-49", "50-59", "Total")
+) |>
+    mutate(happiness = digi |> pull(happiness))
+
+# recreate the plot
+
+df |>
+    ggplot(aes(x = country, y = happiness, fill = group))+
+    geom_col(position = position_dodge()) +
+    labs(
+        x = NULL,
+        y = NULL,
+        fill = NULL
+    ) +
+    coord_cartesian(ylim = c(6, NA), expand = 0)+
+    scale_fill_manual(values = c("#2f74cc", "#ff7502", "#a3d384", "#ffbc00", "#908d90"))+
+    theme_minimal()+
+    theme(
+        legend.position = "bottom",
+        panel.grid.minor = element_blank()
+    )
+
+# custom theming
+devtools::source_gist("653e1040a07364ae82b1bb312501a184")
+sysfonts::font_add_google("Atkinson Hyperlegible", family = "ah")
+theme_set(theme_ik(base_family = "ah"))
+
+
+# Create the plot
+df |>
+    ggplot(aes(y = country, x = happiness, color = group))+
+    geom_point(size = 2) +
+    labs(
+        title = "How happy are you with your home?",
+        subtitle = "Happiness scores from Generations and Gender Programme surveys\non a scale of 0 to 10",
+        x = NULL,
+        y = NULL,
+        color = "Age group"
+    ) +
+    scale_color_manual(values = c("#2f74cc", "#ff7502", "#a3d384", "#ffbc00", "#908d90"))+
+    theme(
+        legend.position = "top",
+        panel.grid.minor.x = element_blank()
+    )+
+    scale_x_continuous(position = "top")
+
